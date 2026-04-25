8da86ec0db4f88c654a401625589f5fb6aa6f2e5
nesterukia <nesterukia@vk.com>
2026-04-25 15:12:45 +0300

    VK-DC-2: fix commit

--- src/main/java/company/vk/edu/distrib/compute/nesterukia/utils/ClusterUtils.java
+++ src/main/java/company/vk/edu/distrib/compute/nesterukia/utils/ClusterUtils.java
@@ -1,9 +1,13 @@
 package company.vk.edu.distrib.compute.nesterukia.utils;
 
-public class ClusterUtils {
-    private static String LOCALHOST_PATTERN = "http://localhost:%d";
+public final class ClusterUtils {
+    private static final String LOCALHOST_PATTERN = "http://localhost:%d";
 
     public static String portToEndpoint(Integer port) {
         return LOCALHOST_PATTERN.formatted(port);
+    }
+
+    private ClusterUtils() {
+        // cannot have instances
     }
 }
